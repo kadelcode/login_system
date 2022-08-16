3d5215556d517e9872625efb88f04a8a279be2e0
kadelcode <kadelcode@gmail.com>
2022-08-16 23:54:21 +0100

    Basic view function for authenticating users

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name = 'register'),
+    path('dashboard/<int:pk>', views.dashboard, name = 'dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
+#dashboard
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
+
 #register view
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        firstname = request.POST['first_name']
+        lastname = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        
+        if password != password1:
+            messages.info(request,'Password does not match')
+            return redirect('register')
+        else:
+            #create a user in the database
+            user = User.objects.create_user(first_name = firstname, last_name = lastname, username = username, email = email, password = password)
+            messages.info(request,'Account successfully created')
+            return redirect('dashboard')
+    else:
+        return render(request, 'accounts/register.html')
