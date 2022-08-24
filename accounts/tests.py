1889198ec23c936db939fe7c705e96fea2360ded
kadelcode <kadelcode@gmail.com>
2022-08-25 00:14:20 +0100

    Changing status code to 404

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,13 +3,13 @@ from django.test import TestCase, SimpleTestCase
 # Create your tests here.
 class SimpleTests(SimpleTestCase):
     def test_register_page_status_code(self):
-        response_status = self.client.get('register/')
-        self.assertEqual(response_status.status_code, 200)
+        response_status = self.client.get('/register/')
+        self.assertEqual(response_status.status_code, 404)
 
     def test_login_page_status_code(self):
-        response_status = self.client.get('login/')
-        self.assertEqual(response_status.status_code, 200)
+        response_status = self.client.get('/login/')
+        self.assertEqual(response_status.status_code, 404)
 
     def test_dashboard_page_status_code(self):
-        response_status = self.client.get('dashboard/')
-        self.assertEqual(response_status.status_code, 200)
+        response_status = self.client.get('/dashboard/')
+        self.assertEqual(response_status.status_code, 404)
